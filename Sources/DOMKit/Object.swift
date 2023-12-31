7fadd1281a2767e2d73dc76ddb420a38252f8b2f
Manolo van Ee <manolo.vanee@gmail.com>
2023-12-31 13:50:06 +0100

    Make Object.constructor optional, which has changed in newer version of JavaScriptKit

--- Sources/DOMKit/Object.swift
+++ Sources/DOMKit/Object.swift
@@ -3,7 +3,7 @@ import JavaScriptKit
 public class Object: JSBridgedClass {
     public let jsObject: JSObject
 
-    public static var constructor: JSFunction { Self.getConstructor() }
+    public static var constructor: JSFunction? { Self.getConstructor() }
 
     public class func getConstructor() -> JSFunction {
         JSObject.global.Object.function!
